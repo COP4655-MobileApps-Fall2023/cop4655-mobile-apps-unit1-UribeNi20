882f7d237c2b557d4a885b775e9bd0baaa27513b
UribeNi20 <nicolasu2002@icloud.com>
2023-09-05 07:06:01 -0400

    Photo of me
    Added photo of me with rounded corners and changed the background app to a dark grey.

--- HW 1/HW 1/ViewController.swift
+++ HW 1/HW 1/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var babyPhoto: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        babyPhoto.layer.cornerRadius = babyPhoto.frame.size.width / 2
+        babyPhoto.clipsToBounds = true
+        babyPhoto.layer.borderColor = UIColor.black.cgColor
+        babyPhoto.layer.borderWidth = 4
     }
 
 
